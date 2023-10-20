bcfbabf0793534719d0e6d9078b0265be9b9729c
Aneesh <aneeshx000@gmail.com>
2023-10-20 20:41:43 +0530

    updated cors

--- server/src/app.js
+++ server/src/app.js
@@ -8,13 +8,32 @@ const appApi = require('./routes/appRoutes/appAPI');
 
 
 // Middleware
+// app.use(
+//   cors({
+//     origin: ["https://api-avengers-frontend.vercel.app"],
+//     methods: ["POST", "GET"],
+//     credentials: true,
+//   })
+// );
+
+const allowedOrigins = ["https://api-avengers-frontend.vercel.app"];
+
+// Configure the cors middleware with the desired options
 app.use(
   cors({
-    origin: ["https://api-avengers-frontend.vercel.app"],
-    methods: ["POST", "GET"],
+    origin: function (origin, callback) {
+      // Check if the origin is in the list of allowed origins
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["POST", "GET", "PATCH"],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(bodyParser.json());
 
